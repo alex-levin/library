bb4010a103d447703d0d0af5762a365d125b33a6
Alex Levin <alexlevinb@gmail.com>
2018-01-14 17:52:39 -0500

    css, js
    	modified:   .jshintrc
    	modified:   bower.json
    	modified:   gulpfile.js
    	modified:   package.json
    	deleted:    public/css/bootstrap.min.css
    	new file:   public/js/app.js
    	deleted:    public/js/bootstrap.min.js
    	modified:   src/views/index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
-var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var jscs = require('gulp-jscs');
+const gulp = require('gulp');
+const jshint = require('gulp-jshint');
+const jscs = require('gulp-jscs');
 
-var jsFiles = ['*.js', 'src/**/*.js'];
+const jsFiles = ['*.js', 'src/**/*.js'];
 
 gulp.task('style', function() {
     return gulp.src(jsFiles)
@@ -12,4 +12,26 @@ gulp.task('style', function() {
         }))
         .pipe(jscs())
         .pipe(jscs.reporter());
+});
+
+gulp.task('inject', function() {
+    const wiredep = require('gulp-wiredep');
+    const inject = require('gulp-inject');
+
+    const injectSrc = gulp.src(['./public/css/*.css',
+        './public/js/*.js'], {read: false});
+
+    const injectOptions = {
+        ignorePath: '/public'
+    };
+
+    const options = {
+        bowerJson: require('./bower.json'),
+        directory: './public/lib',
+        ignorePath: '../../public'
+    }
+    return gulp.src('./src/views/*.html')
+        .pipe(wiredep(options))
+        .pipe(inject(injectSrc, injectOptions))
+        .pipe(gulp.dest('./src/views'));
 });
